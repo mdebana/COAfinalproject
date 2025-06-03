070f890f4c2bafff4a1fa643e86aba2e534b158d
Mochamad Deban Azaria <m.debanazaria@gmail.com>
2025-06-04 05:05:23 +0700

    Run Test

--- cypress/support/pages/agodaPage.js
+++ cypress/support/pages/agodaPage.js
@@ -115,7 +115,9 @@ class AgodaPage {
 	}
 
 	clickNoUpgrade() {
-		cy.get('[data-component="last-chance-decline-button"]').click();
+		cy.get('[data-component="last-chance-decline-button"]',{ timeout: 10000 })
+			.should("be.visible")
+			.click({ force: true });
 	}
 
 	verifyContactName() {
